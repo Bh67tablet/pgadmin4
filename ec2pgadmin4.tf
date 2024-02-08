1846a85a0f7b8f40135e712b1111dc2bd823ee19
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-02-08 08:09:57 +0100

    Update ec2pgadmin4.tf

--- ec2pgadmin4.tf
+++ ec2pgadmin4.tf
@@ -5,6 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2" {
   ami                    = "ami-0355d85befe722c2b"
+  iam_instance_profile   = "ec2-ecr-admin_profile" 
   instance_type          = "t2.micro"
   subnet_id              = "subnet-071f9951d322eedc3"
   associate_public_ip_address = "true"
@@ -12,7 +13,17 @@ resource "aws_instance" "ec2" {
   
 user_data = <<EOF
 #! /bin/bash
+echo "I was here">/var/tmp/greetings.txt
+sudo yum -y update  >>/var/tmp/yum.update 2>&1
+sudo yum -y install docker git
+newgrp docker
+sudo usermod -a -G docker $USER
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo dnf update
+sudo dnf install postgresql15.x86_64 postgresql15-server -y >>/var/tmp/yum.update 2>&1
 sudo su - -c 'su - ec2-user -c "git clone https://github.com/Bh67tablet/pgadmin4.git"' >>/var/tmp/yum.update 2>&1
+sudo su - -c 'su - ec2-user -c /home/ec2-user/pgadmin4/pgadmin4docker.sh' >>/var/tmp/ansiuser.log 2>&1
 EOF
 
   tags = {
