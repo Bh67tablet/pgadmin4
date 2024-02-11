326806b6e9b1c9f28dbec42270ceb0f24db17752
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-02-11 18:28:56 +0100

    Update pgadmin4docker.sh

--- pgadmin4docker.sh
+++ pgadmin4docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker pull dpage/pgadmin4
 newgrp docker
-docker run -it -p 5432:80 \
+docker run -it --restart unless-stopped -p 5432:80 \
     -e "PGADMIN_DEFAULT_EMAIL=bernd.hohmann@vodafone.com" \
     -e "PGADMIN_DEFAULT_PASSWORD=SuperSecret" \
     -d dpage/pgadmin4
