34cda6d7d321587b1c9e3ba80ec5288eaf972367
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-02-11 16:50:13 +0100

    Create initial.sh

---
+++ initial.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "I was here">/var/tmp/greetings.txt
+sudo yum -y update  >>/var/tmp/yum.update 2>&1
+sudo yum -y install docker git
+sudo groupadd docker
+sudo usermod -a -G docker ec2-user
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo dnf update
+sudo dnf install postgresql15.x86_64 postgresql15-server -y >>/var/tmp/yum.update 2>&1
+sudo su - -c 'su - ec2-user -c /home/ec2-user/pgadmin4/pgadmin4docker.sh' >>/var/tmp/ansiuser.log 2>&1
