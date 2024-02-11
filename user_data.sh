d47dbdae896074dce43c5a051150b88ba79f194f
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-02-11 16:37:16 +0100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -2,11 +2,12 @@
 echo "I was here">/var/tmp/greetings.txt
 sudo yum -y update  >>/var/tmp/yum.update 2>&1
 sudo yum -y install docker git
+sudo groupadd docker
+sudo usermod -a -G docker ec2-user
 newgrp docker
-sudo usermod -a -G docker $USER
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo dnf update
 sudo dnf install postgresql15.x86_64 postgresql15-server -y >>/var/tmp/yum.update 2>&1
-sudo su - -c 'su - ec2-user -c "https://github.com/Bh67tablet/pgadmin4.git"' >>/var/tmp/yum.update 2>&1
+sudo su - -c 'su - ec2-user -c "git clone https://github.com/Bh67tablet/pgadmin4.git"' >>/var/tmp/yum.update 2>&1
 sudo su - -c 'su - ec2-user -c /home/ec2-user/pgadmin4/pgadmin4docker.sh' >>/var/tmp/ansiuser.log 2>&1
