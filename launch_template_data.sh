ee0c73158512185f3298c55a7bbfc605cdd183c1
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-02-11 18:37:24 +0100

    Update launch_template_data.sh

--- launch_template_data.sh
+++ launch_template_data.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
-sudo yum -y update  >>/var/tmp/yum.update 2>&1
-sudo yum -y install git
-sudo su - -c 'su - ec2-user -c "git clone https://github.com/Bh67tablet/pgadmin4.git"' >>/var/tmp/yum.update 2>&1
-sudo su - -c 'su - ec2-user -c /home/ec2-user/pgadmin4/initial.sh' >>/var/tmp/yum.update 2>&1
+echo "I was here">/var/tmp/greetings.txt
+sudo yum -y update
+sudo yum -y install docker git
+sudo groupadd docker
+sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo dnf update
+sudo dnf install postgresql15.x86_64 postgresql15-server -y
+sudo su - -c 'su - ec2-user -c newgrp docker'
+sudo su - -c 'su - ec2-user -c docker pull dpage/pgadmin4'
+sudo su - -c 'su - ec2-user -c docker run -it --restart unless-stopped -p 5432:80 -e "PGADMIN_DEFAULT_EMAIL=bernd.hohmann@vodafone.com" -e "PGADMIN_DEFAULT_PASSWORD=SuperSecret" -d dpage/pgadmin4' 
+sudo su - -c 'su - ec2-user -c "git clone https://github.com/Bh67tablet/pgadmin4.git"'
